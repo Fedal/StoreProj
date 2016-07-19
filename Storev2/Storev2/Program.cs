6d87c4fcbaa4635ac50b970770816eec496c0423
teditripon <dorin.tripon@yahoo.com>
2016-07-19 17:37:07 +0300

    FileGenerator added

--- Storev2/Storev2/Program.cs
+++ Storev2/Storev2/Program.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Storev2
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+
+			FileGeneratorService.GenerateFiles();
 			Store store = new Store();
 			InitializeMenu.Show(store);
 			MainMenu.Show(store);
