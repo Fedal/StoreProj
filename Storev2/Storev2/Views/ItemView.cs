eeeb7707ce5d289bc6021b7bd8ce3449060378f8
Fedal <belenesi_david@yahoo.com>
2016-07-21 12:51:19 +0300

    cleaned some stuff

--- Storev2/Storev2/Views/ItemView.cs
+++ Storev2/Storev2/Views/ItemView.cs
@@ -59,18 +59,6 @@ namespace Storev2
 
     internal static void ViewExpirationShelve(Store store)
     {
-      //Console.WriteLine("\tName: {0}", store.Stock.Items[i].Name);
-      //Console.WriteLine("\tCategory: {0}", store.Stock.Items[i].Category);
-      //Console.WriteLine("\tPrice: {0}", store.Stock.Items[i].Price);
-      //Console.WriteLine("\tQuantity: {0}", store.Stock.Items[i].Quantity);
-      //Console.WriteLine("\tExpiration Date: {0}", store.Stock.Items[i].ExpDate);
-      //Console.WriteLine("\tLocation: ");
-      //for (int j = 0; j < store.Stock.Items[i].Location.Count; j++)
-      //{
-      //  Console.WriteLine("\t\t{0}, row {1}, shelve {2}", store.Stock.Items[i].Location[j].Name,
-      //   store.Stock.Items[i].Location[j].Row,
-      //   store.Stock.Items[i].Location[j].Shelve);
-      //}
       int expirationShelveIndex = store.Shop.Layout.Shelves.Length;
       Console.WriteLine("\n=== EXPIRATION SHELVE ITEMS ===");
       for (int i = 0; i < store.Stock.Items.Count; i++ )
