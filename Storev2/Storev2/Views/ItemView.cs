c44196bd8926ea1c1c5579c92a1df8053795b994
Fedal <belenesi_david@yahoo.com>
2016-07-21 12:30:10 +0300

    Added view expiration shelve option

--- Storev2/Storev2/Views/ItemView.cs
+++ Storev2/Storev2/Views/ItemView.cs
@@ -56,5 +56,36 @@ namespace Storev2
       }
       ConfirmView.Show(store);
     }
+
+    internal static void ViewExpirationShelve(Store store)
+    {
+      //Console.WriteLine("\tName: {0}", store.Stock.Items[i].Name);
+      //Console.WriteLine("\tCategory: {0}", store.Stock.Items[i].Category);
+      //Console.WriteLine("\tPrice: {0}", store.Stock.Items[i].Price);
+      //Console.WriteLine("\tQuantity: {0}", store.Stock.Items[i].Quantity);
+      //Console.WriteLine("\tExpiration Date: {0}", store.Stock.Items[i].ExpDate);
+      //Console.WriteLine("\tLocation: ");
+      //for (int j = 0; j < store.Stock.Items[i].Location.Count; j++)
+      //{
+      //  Console.WriteLine("\t\t{0}, row {1}, shelve {2}", store.Stock.Items[i].Location[j].Name,
+      //   store.Stock.Items[i].Location[j].Row,
+      //   store.Stock.Items[i].Location[j].Shelve);
+      //}
+      int expirationShelveIndex = store.Shop.Layout.Shelves.Length;
+      Console.WriteLine("\n=== EXPIRATION SHELVE ITEMS ===");
+      for (int i = 0; i < store.Stock.Items.Count; i++ )
+        for (int j = 0; j < store.Stock.Items[i].Location.Count; j++)
+        {
+          if (store.Stock.Items[i].Location[j].Shelve == store.Shop.Layout.Shelves.Length)
+          {
+            Console.WriteLine("\n\tName: {0}", store.Stock.Items[i].Name);
+            Console.WriteLine("\tCategory: {0}", store.Stock.Items[i].Category);
+            Console.WriteLine("\tPrice: {0}", store.Stock.Items[i].Price);
+            Console.WriteLine("\tQuantity: {0}", store.Stock.Items[i].Quantity);
+            Console.WriteLine("\tExp. Date: {0}\n", store.Stock.Items[i].ExpDate);
+          }
+        }
+      ConfirmView.Show(store);
+    }
   }
 }
